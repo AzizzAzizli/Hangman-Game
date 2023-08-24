c9a820c3335aea4662e8184112717d599ea2d156
AzizAzizli <azizliaziz025@gmail.com>
2023-08-24 16:51:38 +0400

    Game created

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,6 @@
-let arr = ["salam", "sagol"];
-let currentWord = arr[0];
+let arr = ["javascript", "python", "java", "ruby", "go", "kotlin", "php"];
+let j = Math.round(Math.random() * arr.length);
+let currentWord = arr[j];
 
 let newArr = currentWord.split("").map(() => "_");
 
@@ -13,7 +14,6 @@ const keys = document.querySelectorAll(".key");
 keys.forEach((key) => {
   key.addEventListener("click", () => {
     const letter = key.textContent;
-    
 
     let usrInput = letter.toLowerCase(); // Kullanıcının girdiği harfi küçük harfe dönüştür
 
@@ -34,19 +34,72 @@ keys.forEach((key) => {
       });
     } else {
       wrongAttempts++;
-      
+
       console.log(`Incorrect guess. ${6 - wrongAttempts} attempts left.`);
+      if (wrongAttempts < 6) {
+        document.querySelector("#wrongGuess").innerHTML = `Incorrect guess. ${
+          6 - wrongAttempts
+        } attempts left.`;
+      }
     }
     document.querySelector("#outPut").innerHTML = newArr.join(" ");
     console.log(newArr);
 
     if (remainingLetters === 0) {
+      document.querySelector(".rightContent").classList.add("d-none");
+      document.querySelector("#winOrLose").classList.remove("d-none");
+      document.querySelector("#loserCard").classList.add("d-none");
+      document.querySelector(
+        "#cardContentWin"
+      ).innerHTML = `Congratulations! You've guessed the word.`;
+
       console.log("Congratulations! You've guessed the word.");
     }
 
     if (wrongAttempts >= 6) {
+      document.querySelector(".rightContent").classList.add("d-none");
+      document.querySelector("#winOrLose").classList.remove("d-none");
+      document.querySelector("#winnerCard").classList.add("d-none");
+      document.querySelector(
+        "#cardContentLost"
+      ).innerHTML = `Out of attempts. The word was: ${currentWord}`;
+
       console.log("Out of attempts. The word was:", currentWord);
     }
-  
   });
+});
+
+const tryAgainButton = document.querySelector("#tryAgainButtonLose");
+tryAgainButton.addEventListener("click", () => {
+  // Yeniden başlatmak için gerekli kodları burada çağırın
+  currentWord = arr[Math.floor(Math.random() * arr.length)];
+  newArr = currentWord.split("").map(() => "_");
+  remainingLetters = currentWord.length;
+  wrongAttempts = 0;
+
+  document.querySelector(".rightContent").classList.remove("d-none");
+  document.querySelector("#winOrLose").classList.add("d-none");
+  // document.querySelector("#cardContentLost").textContent = "";
+  document.querySelector("#wrongGuess").textContent =
+    "Incorrect guess. 6 attempts left.";
+  document.querySelector("#outPut").textContent = newArr.join(" ");
+  document.querySelector("#winnerCard").classList.remove("d-none");
+  document.querySelector("#loserCard").classList.remove("d-none");
+});
+const tryAgainButtonwin = document.querySelector("#tryAgainButtonWin");
+tryAgainButtonwin.addEventListener("click", () => {
+  // Yeniden başlatmak için gerekli kodları burada çağırın
+  currentWord = arr[Math.floor(Math.random() * arr.length)];
+  newArr = currentWord.split("").map(() => "_");
+  remainingLetters = currentWord.length;
+  wrongAttempts = 0;
+
+  document.querySelector(".rightContent").classList.remove("d-none");
+  document.querySelector("#winOrLose").classList.add("d-none");
+  // document.querySelector("#cardContentWin").textContent = "";
+  document.querySelector("#wrongGuess").textContent =
+    "Incorrect guess. 6 attempts left.";
+  document.querySelector("#outPut").textContent = newArr.join(" ");
+  document.querySelector("#winnerCard").classList.remove("d-none");
+  document.querySelector("#loserCard").classList.remove("d-none");
 });
